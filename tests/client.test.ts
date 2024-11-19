6b29e474a397b99239356f9e4e680ee88f6c05d3
Lionello Lunesu <lio+git@lunesu.com>
2024-11-19 12:56:07 -0800

    Fix client test

--- tests/client.test.ts
+++ tests/client.test.ts
@@ -7,6 +7,7 @@ describe("#newClient", () => {
     const client = newClient("fabric-prod1.defang.dev", "");
 
     const trackRequest = new pb.TrackRequest();
+    trackRequest.setAnonId("6046178C-AD7C-45E2-957A-5DA4BF6D2A8C"); // avoid segment error
     trackRequest.setEvent("Pulumi-Defang Test");
     client.track(trackRequest, (err) => (err ? done(err) : done()));
   });
