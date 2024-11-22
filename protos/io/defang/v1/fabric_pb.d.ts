b6931423dd219d7676108bdec716e9d70f775cf8
Lionello Lunesu <lio+git@lunesu.com>
2024-11-22 13:31:15 -0800

    Re-add ServiceID for old provider store in state

--- protos/io/defang/v1/fabric_pb.d.ts
+++ protos/io/defang/v1/fabric_pb.d.ts
@@ -1250,6 +1250,30 @@ export namespace GetRequest {
   }
 }
 
+export class ServiceID extends jspb.Message {
+  getName(): string;
+  setName(value: string): void;
+
+  getProject(): string;
+  setProject(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ServiceID.AsObject;
+  static toObject(includeInstance: boolean, msg: ServiceID): ServiceID.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ServiceID, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ServiceID;
+  static deserializeBinaryFromReader(message: ServiceID, reader: jspb.BinaryReader): ServiceID;
+}
+
+export namespace ServiceID {
+  export type AsObject = {
+    name: string,
+    project: string,
+  }
+}
+
 export class Device extends jspb.Message {
   clearCapabilitiesList(): void;
   getCapabilitiesList(): Array<string>;
