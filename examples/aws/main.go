b67a19c936b7b97f9ad27dabf5c47308c92fa3fa
jordanstephens <jordan@stephens.io>
2026-03-17 15:08:24 -0700

    add more services

--- examples/aws/main.go
+++ examples/aws/main.go
@@ -10,23 +10,115 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		proj, err := defangaws.NewProject(ctx, "myProject", &defangaws.ProjectArgs{
 			Services: shared.ServiceInputMap{
-				"web": shared.ServiceInputArgs{
-					Image: pulumi.StringPtr("nginx:latest"),
+				"app": shared.ServiceInputArgs{
+					Build: shared.BuildInputArgs{
+						Context:    pulumi.String("."),
+						Dockerfile: pulumi.StringPtr("Dockerfile"),
+					},
+					Command: pulumi.StringArray{pulumi.String("./run.sh")},
 					Ports: shared.PortConfigArray{
 						shared.PortConfigArgs{
-							Target:      pulumi.Int(80),
+							Target:      pulumi.Int(8000),
 							Mode:        pulumi.StringPtr("ingress"),
 							AppProtocol: pulumi.StringPtr("http"),
 						},
 					},
-				},
-				"db": shared.ServiceInputArgs{
-					Image:    pulumi.StringPtr("postgres:16"),
-					Postgres: shared.PostgresInputArgs{},
 					Environment: pulumi.StringMap{
-						"POSTGRES_USER":     pulumi.String("defang"),
-						"POSTGRES_PASSWORD": pulumi.String("secret"),
-						"POSTGRES_DB":       pulumi.String("mydb"),
+						"DJANGO_SETTINGS_MODULE": pulumi.String("config.settings"),
+						// TODO: interpolate POSTGRES_PASSWORD from config/secret
+						"DATABASE_URL":          pulumi.String("postgres://postgres:@postgres:5432/postgres?sslmode=require"),
+						"REDIS_URL":             pulumi.String("redis://redis:6379/0"),
+						"CELERY_BROKER_URL":     pulumi.String("redis://redis:6379/0"),
+						"CELERY_RESULT_BACKEND": pulumi.String("redis://redis:6379/0"),
+						"DJANGO_SECRET_KEY":     pulumi.String(""), // set via config/secret
+						"SSL_MODE":              pulumi.String(""), // set via config/secret
+					},
+					HealthCheck: shared.HealthCheckConfigArgs{
+						Test:               pulumi.StringArray{pulumi.String("CMD"), pulumi.String("curl"), pulumi.String("-f"), pulumi.String("http://localhost:8000/")},
+						IntervalSeconds:    pulumi.IntPtr(5),
+						TimeoutSeconds:     pulumi.IntPtr(2),
+						Retries:            pulumi.IntPtr(10),
+						StartPeriodSeconds: pulumi.IntPtr(10),
+					},
+					Deploy: shared.DeployConfigArgs{
+						Resources: shared.ResourcesConfigArgs{
+							Reservations: shared.ResourceConfigArgs{
+								Cpus:   pulumi.Float64Ptr(0.5),
+								Memory: pulumi.StringPtr("512M"),
+							},
+						},
+					},
+				},
+				"worker": shared.ServiceInputArgs{
+					Build: shared.BuildInputArgs{
+						Context:    pulumi.String("."),
+						Dockerfile: pulumi.StringPtr("Dockerfile"),
+					},
+					Command: pulumi.StringArray{
+						pulumi.String("celery"),
+						pulumi.String("-A"),
+						pulumi.String("config"),
+						pulumi.String("worker"),
+						pulumi.String("-l"),
+						pulumi.String("info"),
+					},
+					Environment: pulumi.StringMap{
+						"DJANGO_SETTINGS_MODULE": pulumi.String("config.settings"),
+						// TODO: interpolate POSTGRES_PASSWORD from config/secret
+						"DATABASE_URL":      pulumi.String("postgres://postgres:@postgres:5432/postgres?sslmode=require"),
+						"REDIS_URL":         pulumi.String("redis://redis:6379/0"),
+						"OPENAI_API_KEY":    pulumi.String("defang"),
+						"DJANGO_SECRET_KEY": pulumi.String(""), // set via config/secret
+						"SSL_MODE":          pulumi.String(""), // set via config/secret
+					},
+					Deploy: shared.DeployConfigArgs{
+						Resources: shared.ResourcesConfigArgs{
+							Reservations: shared.ResourceConfigArgs{
+								Cpus:   pulumi.Float64Ptr(0.5),
+								Memory: pulumi.StringPtr("512M"),
+							},
+						},
+					},
+				},
+				"postgres": shared.ServiceInputArgs{
+					Image:    pulumi.StringPtr("pgvector/pgvector:pg16"),
+					Postgres: shared.PostgresInputArgs{},
+					Ports: shared.PortConfigArray{
+						shared.PortConfigArgs{
+							Target: pulumi.Int(5432),
+							Mode:   pulumi.StringPtr("host"),
+						},
+					},
+					Environment: pulumi.StringMap{
+						"POSTGRES_PASSWORD": pulumi.String(""), // set via config/secret
+					},
+					Deploy: shared.DeployConfigArgs{
+						Resources: shared.ResourcesConfigArgs{
+							Reservations: shared.ResourceConfigArgs{
+								Cpus:   pulumi.Float64Ptr(0.5),
+								Memory: pulumi.StringPtr("512M"),
+							},
+						},
+					},
+				},
+				// x-defang-redis: Redis managed service not yet supported in Go SDK;
+				// deployed as a regular container image.
+				"redis": shared.ServiceInputArgs{
+					Image: pulumi.StringPtr("redis:6.2"),
+					// Redis: shared.RedisInputArgs{},
+					Ports: shared.PortConfigArray{
+						shared.PortConfigArgs{
+							Target: pulumi.Int(6379),
+							Mode:   pulumi.StringPtr("host"),
+						},
+					},
+					Deploy: shared.DeployConfigArgs{
+						Resources: shared.ResourcesConfigArgs{
+							Reservations: shared.ResourceConfigArgs{
+								Cpus:   pulumi.Float64Ptr(0.5),
+								Memory: pulumi.StringPtr("512M"),
+							},
+						},
 					},
 				},
 			},
