d3f310f31c44a4d5b5a98a10de4a1374b24cf1e6
jordanstephens <jordan@stephens.io>
2026-03-23 13:18:02 -0700

    inline azure Build

--- provider/defangazure/project.go
+++ provider/defangazure/project.go
@@ -3,9 +3,10 @@ package defangazure
 import (
 	"fmt"
 
-	"github.com/DefangLabs/pulumi-defang/provider/common"
 	"github.com/DefangLabs/pulumi-defang/provider/compose"
 	providerazure "github.com/DefangLabs/pulumi-defang/provider/defangazure/azure"
+	"github.com/pulumi/pulumi-azure-native-sdk/app/v2"
+	"github.com/pulumi/pulumi-azure-native-sdk/resources/v2"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
@@ -38,21 +39,81 @@ func (*Project) Construct(ctx *pulumi.Context, name, typ string, inputs ProjectI
 	}
 
 	childOpt := pulumi.Parent(comp)
-	args := common.BuildArgs{
-		Services: inputs.Services,
-	}
+	childOpts := []pulumi.ResourceOption{childOpt}
 
-	result, err := providerazure.Build(ctx, name, args, childOpt)
+	location := providerazure.Location(ctx)
+
+	rg, err := resources.NewResourceGroup(ctx, name, &resources.ResourceGroupArgs{
+		Location: pulumi.String(location),
+	}, childOpts...)
 	if err != nil {
-		return nil, fmt.Errorf("failed to build Azure resources: %w", err)
+		return nil, fmt.Errorf("creating resource group: %w", err)
 	}
 
-	comp.Endpoints = result.Endpoints
-	comp.LoadBalancerDNS = result.LoadBalancerDNS
+	env, err := app.NewManagedEnvironment(ctx, name, &app.ManagedEnvironmentArgs{
+		ResourceGroupName: rg.Name,
+		Location:          pulumi.String(location),
+	}, childOpts...)
+	if err != nil {
+		return nil, fmt.Errorf("creating managed environment: %w", err)
+	}
+
+	infra := &providerazure.SharedInfra{
+		ResourceGroup: rg,
+		Environment:   env,
+	}
+
+	type serviceComponent struct{ pulumi.ResourceState }
+	endpoints := pulumi.StringMap{}
+
+	for svcName, svc := range inputs.Services {
+		comp := &serviceComponent{}
+
+		if svc.Postgres != nil {
+			if err := ctx.RegisterComponentResource("defang-azure:index:Postgres", svcName, comp, childOpts...); err != nil {
+				return nil, fmt.Errorf("registering Azure Postgres component %s: %w", svcName, err)
+			}
+			svcOpts := []pulumi.ResourceOption{pulumi.Parent(comp)}
+
+			configProvider := providerazure.NewConfigProvider(name)
+			pgResult, err := providerazure.CreatePostgresFlexible(ctx, configProvider, svcName, svc, infra, svcOpts...)
+			if err != nil {
+				return nil, fmt.Errorf("creating PostgreSQL for %s: %w", svcName, err)
+			}
+			endpoints[svcName] = pulumi.Sprintf("%s:5432", pgResult.Server.FullyQualifiedDomainName)
+		} else {
+			if err := ctx.RegisterComponentResource("defang-azure:index:AzureContainerApp", svcName, comp, childOpts...); err != nil {
+				return nil, fmt.Errorf("registering Container App component %s: %w", svcName, err)
+			}
+			svcOpts := []pulumi.ResourceOption{pulumi.Parent(comp)}
+
+			caResult, err := providerazure.CreateContainerApp(ctx, svcName, svc, infra, svcOpts...)
+			if err != nil {
+				return nil, fmt.Errorf("creating Container App %s: %w", svcName, err)
+			}
+			endpoints[svcName] = caResult.App.LatestRevisionFqdn.ApplyT(func(fqdn string) string {
+				if fqdn != "" {
+					return fmt.Sprintf("https://%s", fqdn)
+				}
+				return ""
+			}).(pulumi.StringOutput)
+		}
+
+		if err := ctx.RegisterResourceOutputs(comp, pulumi.Map{
+			"endpoint": endpoints[svcName],
+		}); err != nil {
+			return nil, fmt.Errorf("registering outputs for %s: %w", svcName, err)
+		}
+	}
+
+	loadBalancerDNS := pulumi.StringPtr("").ToStringPtrOutput()
+
+	comp.Endpoints = endpoints.ToStringMapOutput()
+	comp.LoadBalancerDNS = loadBalancerDNS
 
 	if err := ctx.RegisterResourceOutputs(comp, pulumi.Map{
-		"endpoints":       result.Endpoints,
-		"loadBalancerDns": result.LoadBalancerDNS,
+		"endpoints":       endpoints.ToStringMapOutput(),
+		"loadBalancerDns": loadBalancerDNS,
 	}); err != nil {
 		return nil, err
 	}
