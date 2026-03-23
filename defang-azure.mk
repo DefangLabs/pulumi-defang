0d9feb1d7e364a27283587f227e26bb4944c5df3
jordanstephens <jordan@stephens.io>
2026-03-23 15:58:04 -0700

    fix concurrent sdk builds
    so we can run make -j3 sdks

--- defang-azure.mk
+++ defang-azure.mk
@@ -38,50 +38,62 @@ version:
 	@echo $(VERSION)
 
 .PHONY: go_sdk
-go_sdk: provider
+go_sdk: .sdk.go.$(PACK).stamp
+
+.sdk.go.$(PACK).stamp: $(WORKING_DIR)/bin/$(PROVIDER)
 	rm -rf "$(SDK_GO_DIR)"
-	pulumi package gen-sdk "$(WORKING_DIR)/bin/$(PROVIDER)" --language go -o "$(WORKING_DIR)/.sdk.tmp"
-	mkdir -p "$(dir $(SDK_GO_DIR))" && mv "$(WORKING_DIR)/.sdk.tmp/go/defangazure" "$(SDK_GO_DIR)" && rm -rf "$(WORKING_DIR)/.sdk.tmp"
+	pulumi package gen-sdk "$(WORKING_DIR)/bin/$(PROVIDER)" --language go -o "$(WORKING_DIR)/.sdk.tmp.$(PACK)"
+	mkdir -p "$(dir $(SDK_GO_DIR))" && mv "$(WORKING_DIR)/.sdk.tmp.$(PACK)/go/defangazure" "$(SDK_GO_DIR)" && rm -rf "$(WORKING_DIR)/.sdk.tmp.$(PACK)"
 	cd "$(SDK_GO_DIR)" && go mod init "$(SDK_MODULE)" && \
 		go get "github.com/pulumi/pulumi/sdk/v3@$(shell grep 'pulumi/pulumi/sdk/v3 ' $(WORKING_DIR)/go.mod | awk '{print $$2}')" && \
 		go mod tidy
+	@touch $@
 
 nodejs_sdk: VERSION := $(shell pulumictl get version --language javascript $(if $(filter 0,$(IS_PRERELEASE)),--is-prerelease) | sed 's/^v//')
 .PHONY: nodejs_sdk
-nodejs_sdk: provider
+nodejs_sdk: .sdk.nodejs.$(PACK).stamp
+
+.sdk.nodejs.$(PACK).stamp: $(WORKING_DIR)/bin/$(PROVIDER)
 	rm -rf "sdk/nodejs/${PACK}"
-	pulumi package gen-sdk "$(WORKING_DIR)/bin/$(PROVIDER)" --language nodejs -o "$(WORKING_DIR)/.sdk.tmp"
-	mkdir -p "sdk/nodejs/${PACK}" && cp -r "$(WORKING_DIR)/.sdk.tmp/nodejs/." "sdk/nodejs/${PACK}" && rm -rf "$(WORKING_DIR)/.sdk.tmp"
+	pulumi package gen-sdk "$(WORKING_DIR)/bin/$(PROVIDER)" --language nodejs -o "$(WORKING_DIR)/.sdk.tmp.$(PACK)"
+	mkdir -p "sdk/nodejs/${PACK}" && cp -r "$(WORKING_DIR)/.sdk.tmp.$(PACK)/nodejs/." "sdk/nodejs/${PACK}" && rm -rf "$(WORKING_DIR)/.sdk.tmp.$(PACK)"
 	cd "${PACKDIR}/nodejs/${PACK}/" && \
 		yarn install && \
 		yarn run tsc && \
 		sed -i.bak 's/$${VERSION}/$(VERSION)/g' package.json && \
 		rm -f ./package.json.bak && \
 		cp ../../../README.md ../../../LICENSE package.json yarn.lock bin/
+	@touch $@
 
 python_sdk: PYPI_VERSION := $(shell pulumictl get version --language python $(if $(filter 0,$(IS_PRERELEASE)),--is-prerelease))
 .PHONY: python_sdk
-python_sdk: provider
+python_sdk: .sdk.python.$(PACK).stamp
+
+.sdk.python.$(PACK).stamp: $(WORKING_DIR)/bin/$(PROVIDER)
 	rm -rf "sdk/python/${PACK}"
-	pulumi package gen-sdk "$(WORKING_DIR)/bin/$(PROVIDER)" --language python -o "$(WORKING_DIR)/.sdk.tmp"
-	mkdir -p "sdk/python/${PACK}" && cp -r "$(WORKING_DIR)/.sdk.tmp/python/." "sdk/python/${PACK}" && rm -rf "$(WORKING_DIR)/.sdk.tmp"
+	pulumi package gen-sdk "$(WORKING_DIR)/bin/$(PROVIDER)" --language python -o "$(WORKING_DIR)/.sdk.tmp.$(PACK)"
+	mkdir -p "sdk/python/${PACK}" && cp -r "$(WORKING_DIR)/.sdk.tmp.$(PACK)/python/." "sdk/python/${PACK}" && rm -rf "$(WORKING_DIR)/.sdk.tmp.$(PACK)"
 	cp README.md "${PACKDIR}/python/${PACK}/"
 	cd "${PACKDIR}/python/${PACK}/" && \
 		python3 setup.py clean --all 2>/dev/null; \
-		rm -rf ./bin/ ../python.bin/ && cp -R . ../python.bin && mv ../python.bin ./bin && \
+		rm -rf ./bin/ ../python.bin.$(PACK)/ && cp -R . ../python.bin.$(PACK) && mv ../python.bin.$(PACK) ./bin && \
 		sed -i.bak -e 's/^VERSION = .*/VERSION = "$(PYPI_VERSION)"/g' -e 's/^PLUGIN_VERSION = .*/PLUGIN_VERSION = "$(VERSION)"/g' ./bin/setup.py && \
 		rm -f ./bin/setup.py.bak && \
 		cd ./bin && python3 setup.py build sdist
+	@touch $@
 
 dotnet_sdk: DOTNET_VERSION := $(shell pulumictl get version --language dotnet $(if $(filter 0,$(IS_PRERELEASE)),--is-prerelease))
 .PHONY: dotnet_sdk
-dotnet_sdk: provider
+dotnet_sdk: .sdk.dotnet.$(PACK).stamp
+
+.sdk.dotnet.$(PACK).stamp: $(WORKING_DIR)/bin/$(PROVIDER)
 	rm -rf "sdk/dotnet/${PACK}"
-	pulumi package gen-sdk "$(WORKING_DIR)/bin/$(PROVIDER)" --language dotnet -o "$(WORKING_DIR)/.sdk.tmp"
-	mkdir -p "sdk/dotnet/${PACK}" && cp -r "$(WORKING_DIR)/.sdk.tmp/dotnet/." "sdk/dotnet/${PACK}" && rm -rf "$(WORKING_DIR)/.sdk.tmp"
+	pulumi package gen-sdk "$(WORKING_DIR)/bin/$(PROVIDER)" --language dotnet -o "$(WORKING_DIR)/.sdk.tmp.$(PACK)"
+	mkdir -p "sdk/dotnet/${PACK}" && cp -r "$(WORKING_DIR)/.sdk.tmp.$(PACK)/dotnet/." "sdk/dotnet/${PACK}" && rm -rf "$(WORKING_DIR)/.sdk.tmp.$(PACK)"
 	cd "${PACKDIR}/dotnet/${PACK}/" && \
 		echo "${DOTNET_VERSION}" >version.txt && \
 		dotnet build /p:Version=${DOTNET_VERSION}
+	@touch $@
 
 .PHONY: sdks
 sdks: go_sdk nodejs_sdk python_sdk dotnet_sdk
@@ -96,3 +108,4 @@ install: provider
 .PHONY: clean
 clean:
 	rm -rf "$(WORKING_DIR)/bin/${PROVIDER}" "$(SDK_GO_DIR)" "sdk/nodejs/${PACK}" "sdk/python/${PACK}" "sdk/dotnet/${PACK}"
+	rm -f .sdk.go.$(PACK).stamp .sdk.nodejs.$(PACK).stamp .sdk.python.$(PACK).stamp .sdk.dotnet.$(PACK).stamp
