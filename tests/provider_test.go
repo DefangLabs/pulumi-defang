d2c7c0ffb86a928acb57dd1199a5a18c483d699a
jordanstephens <jordan@stephens.io>
2026-03-23 14:00:33 -0700

    remove unused function

--- tests/provider_test.go
+++ tests/provider_test.go
@@ -3,18 +3,10 @@ package tests
 import (
 	"testing"
 
-	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
 	"github.com/stretchr/testify/require"
 )
 
 func TestProviderStarts(t *testing.T) {
 	server := makeTestServer()
 	require.NotNil(t, server)
-}
-
-// urn is a helper function to build an urn for running integration tests.
-func urn(typ string) resource.URN {
-	return resource.NewURN("stack", "proj", "",
-		tokens.Type("test:index:"+typ), "name")
 }
