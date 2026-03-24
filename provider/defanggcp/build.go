e01f58444033916dc4a81235dfafbb84eadf5dcb
jordanstephens <jordan@stephens.io>
2026-03-24 15:10:31 -0700

    cp build.go from gcpcd

---
+++ provider/defanggcp/build.go
@@ -0,0 +1,240 @@
+package defanggcp
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math"
+	"net/url"
+	"strings"
+	"time"
+
+	cloudbuild "cloud.google.com/go/cloudbuild/apiv1"
+	cloudbuildpb "cloud.google.com/go/cloudbuild/apiv1/v2/cloudbuildpb"
+	"cloud.google.com/go/longrunning"
+	lroauto "cloud.google.com/go/longrunning/autogen"
+	"cloud.google.com/go/longrunning/autogen/longrunningpb"
+	"github.com/pulumi/pulumi-go-provider/infer"
+	"google.golang.org/api/option"
+	gtransport "google.golang.org/api/transport/grpc"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/durationpb"
+	"gopkg.in/yaml.v3"
+)
+
+type CloudBuild struct{}
+
+type CloudBuildArgs struct {
+	// Required fields
+	ProjectId string `pulumi:"projectId" provider:"replaceOnChanges"`
+	Location  string `pulumi:"location" provider:"replaceOnChanges"`
+	Source    string `pulumi:"source" provider:"replaceOnChanges"`
+	Steps     string `pulumi:"steps" provider:"replaceOnChanges"`
+
+	// TODO: We should be able to use ETAG from object metadata as ditest in Diff func to determine a new build is necessary
+	SourceDigest   *string           `pulumi:"sourceDigest,optional" provider:"replaceOnChanges"`
+	Images         []string          `pulumi:"images,optional" provider:"replaceOnChanges"`
+	ServiceAccount *string           `pulumi:"serviceAccount,optional" provider:"replaceOnChanges"`
+	Tags           []string          `pulumi:"tags,optional"`
+	MachineType    *string           `pulumi:"machineType,optional"`
+	DiskSizeGb     *int64            `pulumi:"diskSizeGb,optional"`
+	Substitutions  map[string]string `pulumi:"substitutions,optional"`
+
+	// Deprecated fields for compatibility with older builds
+	Image *string `pulumi:"image,optional"`
+	// Deprecated fields for compatibility with older builds
+	CmdArgs []string `pulumi:"cmdArgs,optional"`
+}
+
+type MachineType = cloudbuildpb.BuildOptions_MachineType
+
+const (
+	UNSPECIFIED   MachineType = cloudbuildpb.BuildOptions_UNSPECIFIED
+	N1_HIGHCPU_8  MachineType = cloudbuildpb.BuildOptions_N1_HIGHCPU_8
+	N1_HIGHCPU_32 MachineType = cloudbuildpb.BuildOptions_N1_HIGHCPU_32
+	E2_HIGHCPU_8  MachineType = cloudbuildpb.BuildOptions_E2_HIGHCPU_8
+	E2_HIGHCPU_32 MachineType = cloudbuildpb.BuildOptions_E2_HIGHCPU_32
+	E2_MEDIUM     MachineType = cloudbuildpb.BuildOptions_E2_MEDIUM
+)
+
+type CloudBuildState struct {
+	CloudBuildArgs
+	BuildId     string `pulumi:"buildId"`
+	ImageDigest string `pulumi:"imageDigest"`
+}
+
+// All resources must implement Create at a minumum.
+func (*CloudBuild) Create(
+	ctx context.Context,
+	req infer.CreateRequest[CloudBuildArgs],
+) (infer.CreateResponse[CloudBuildState], error) {
+	state := CloudBuildState{CloudBuildArgs: req.Inputs}
+	if req.DryRun {
+		return infer.CreateResponse[CloudBuildState]{ID: req.Name, Output: state}, nil
+	}
+	buildId, imageDigest, err := runCloudBuild(ctx, req.Inputs)
+	if err != nil {
+		return infer.CreateResponse[CloudBuildState]{ID: req.Name, Output: state}, err
+	}
+	state.BuildId = buildId
+	state.ImageDigest = imageDigest
+	return infer.CreateResponse[CloudBuildState]{ID: req.Name, Output: state}, nil
+}
+
+func runCloudBuild(ctx context.Context, args CloudBuildArgs) (string, string, error) {
+	client, err := cloudbuild.NewClient(ctx)
+	if err != nil {
+		return "", "", fmt.Errorf("failed to create Cloud Build client: %w", err)
+	}
+	defer client.Close()
+
+	// steps := createSteps(args)
+	var steps []*cloudbuildpb.BuildStep
+	if err := yaml.Unmarshal([]byte(args.Steps), &steps); err != nil {
+		return "", "", fmt.Errorf("failed to parse cloudbuild steps: %w, steps are:\n%v\n", err, args.Steps)
+	}
+
+	// Extract bucket and object from the source
+	bucket, object, err := parseGCSURI(args.Source)
+	if err != nil {
+		return "", "", fmt.Errorf("failed to parse source URI: %w", err)
+	}
+
+	// TODO: Implement secrets with a global `availableSecrets` and per-step `secretEnv`
+	// See: https://cloud.google.com/build/docs/securing-builds/use-secrets
+	// TODO: Use inline secret for environment variables since there is no other way to pass env vars to build steps
+	// var secrets *cloudbuildpb.Secrets
+
+	// Create a build request
+	build := &cloudbuildpb.Build{
+		Substitutions: args.Substitutions,
+		Steps:         steps,
+		// TODO: Support NPM or Python packages using Artifacts field
+		Source: &cloudbuildpb.Source{
+			Source: &cloudbuildpb.Source_StorageSource{
+				StorageSource: &cloudbuildpb.StorageSource{
+					Bucket: bucket,
+					Object: object,
+				},
+			},
+		},
+		// AvailableSecrets: secrets,
+		Options: &cloudbuildpb.BuildOptions{
+			MachineType: GetMachineType(args.MachineType),
+			DiskSizeGb:  GetDiskSize(args.DiskSizeGb),
+			Logging:     cloudbuildpb.BuildOptions_CLOUD_LOGGING_ONLY,
+		},
+		Timeout: durationpb.New(time.Hour),
+		Tags:    args.Tags,
+	}
+
+	if args.ServiceAccount != nil {
+		build.ServiceAccount = fmt.Sprintf("projects/%s/serviceAccounts/%s", args.ProjectId, *args.ServiceAccount)
+	}
+
+	if args.Images != nil {
+		build.Images = args.Images
+	}
+
+	// Trigger the build
+	pbop, err := client.CreateBuild(ctx, &cloudbuildpb.CreateBuildRequest{
+		ProjectId: args.ProjectId, // Replace with your GCP project ID
+		// Current API endpoint does not support location
+		// Parent:    fmt.Sprintf("projects/%s/locations/%s", args.ProjectId, args.Location),
+		Build: build,
+	})
+	if err != nil {
+		return "", "", fmt.Errorf("failed to create build: %w", err)
+	}
+
+	op, err := NewCloudBuildOperation(ctx, pbop)
+	if err != nil {
+		return "", "", fmt.Errorf("failed to create Cloud Build operation: %w", err)
+	}
+
+	build, err = op.Wait(ctx)
+	if err != nil {
+		return "", "", fmt.Errorf("failed to wait for build to complete: %w", err)
+	}
+	if build.Status != cloudbuildpb.Build_SUCCESS {
+		return "", "", fmt.Errorf("build failed: %v: %v", build.Status.String(), build.StatusDetail)
+	}
+	if build.Results == nil {
+		return "", "", errors.New("build results are missing")
+	}
+	if len(build.Results.Images) == 0 {
+		return "", "", errors.New("build results are missing images")
+	}
+	return build.Id, build.Results.Images[0].Digest, nil
+}
+
+type CloudBuildOperation struct {
+	lro *longrunning.Operation
+}
+
+func NewCloudBuildOperation(ctx context.Context, op *longrunningpb.Operation) (*CloudBuildOperation, error) {
+
+	clientOptions := []option.ClientOption{
+		option.WithEndpoint("cloudbuild.googleapis.com:443"),
+		option.WithScopes("https://www.googleapis.com/auth/cloud-platform"),
+		option.WithGRPCDialOption(grpc.WithDefaultCallOptions(
+			grpc.MaxCallRecvMsgSize(math.MaxInt32))),
+	}
+
+	connPool, err := gtransport.DialPool(ctx, clientOptions...)
+	if err != nil {
+		return nil, err
+	}
+
+	lroClient, err := lroauto.NewOperationsClient(ctx, gtransport.WithConnPool(connPool))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Operations client: %w", err)
+	}
+	lro := longrunning.InternalNewOperation(lroClient, op)
+
+	return &CloudBuildOperation{lro: lro}, nil
+}
+
+func (o *CloudBuildOperation) Wait(ctx context.Context) (*cloudbuildpb.Build, error) {
+	var resp cloudbuildpb.Build
+	err := o.lro.Wait(ctx, &resp)
+	if err != nil {
+		return nil, err
+	}
+	return &resp, err
+}
+
+func GetMachineType(machineType *string) MachineType {
+	if machineType == nil {
+		return UNSPECIFIED
+	}
+	m, ok := cloudbuildpb.BuildOptions_MachineType_value[*machineType]
+	if !ok {
+		return UNSPECIFIED
+	}
+	return MachineType(m)
+}
+
+func GetDiskSize(diskSizeGb *int64) int64 {
+	if diskSizeGb == nil {
+		return 0
+	}
+	return *diskSizeGb
+}
+
+func parseGCSURI(uri string) (bucket string, object string, err error) {
+	if !strings.HasPrefix(uri, "gs://") {
+		return "", "", errors.New("URI must start with 'gs://' prefix")
+	}
+
+	parts := strings.SplitN(uri[5:], "/", 2)
+	if len(parts) < 2 {
+		return "", "", errors.New("URI must contain a bucket and an object path")
+	}
+	obj, err := url.QueryUnescape(parts[1]) // Because the base 64 encoding may contain '='
+	if err != nil {
+		return "", "", fmt.Errorf("failed to unescape object path: %w", err)
+	}
+
+	return parts[0], obj, nil
+}
